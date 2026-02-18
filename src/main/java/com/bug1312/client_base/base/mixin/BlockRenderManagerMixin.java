6e92c6ac527f8557ac2311d7ad03c4a15dad84e3
Bug1312 <bug@bug1312.com>
2026-02-17 17:24:37 -0700

    replace_state renderer being active when client base disabled and vice versa

--- src/main/java/com/bug1312/client_base/base/mixin/BlockRenderManagerMixin.java
+++ src/main/java/com/bug1312/client_base/base/mixin/BlockRenderManagerMixin.java
@@ -30,7 +30,7 @@ abstract class BlockRenderManagerMixin {
 
 	@Inject(method = "getModel", at = @At("HEAD"), cancellable = true)
 	private void client_base$blockModelRendererReplace(BlockState state, CallbackInfoReturnable<BakedModel> ci) {
-		if (ClientBaseApi.isBaseActive()) return;
+		if (!ClientBaseApi.isBaseActive()) return;
 
 		for (var entry : ClientBaseConfig.getInstance().renderers().entrySet()) {
 			if (!(entry.getValue() instanceof BlockModelRenderer blockModelRenderer)) continue;
